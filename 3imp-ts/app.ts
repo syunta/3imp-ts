58dc0067afb2f22d10dde785876ee14b992ba0c8
syunta kitagawa <kitagawa@timedia.co.jp>
2016-01-23 19:51:35 +0900

    Add cadr

--- 3imp-ts/app.ts
+++ 3imp-ts/app.ts
@@ -20,8 +20,13 @@ function cons<TCar, TCdr>(car: TCar, cdr: TCdr) {
     return new Cell(car, cdr);
 }
 
+function cadr<TCar1, TCar2, TCdr>(cell: Cell<TCar1, Cell<TCar2, TCdr>>) {
+    return car(cdr(cell));
+}
+
 window.onload = () => {
     var el = window.document.getElementById('content');
     var cell = cons(1, cons(cons("hoge", 10), 3));
     el.innerHTML += car(cell);
+    el.innerHTML += cadr(cell).car;
 };
