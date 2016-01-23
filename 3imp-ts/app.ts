28052e3659b4ed3ceac58ea441cd281c93cce6bb
syunta kitagawa <kitagawa@timedia.co.jp>
2016-01-23 19:48:11 +0900

    Add Cell

--- 3imp-ts/app.ts
+++ 3imp-ts/app.ts
@@ -1,2 +1,12 @@
-﻿window.onload = () => {
+﻿class Cell<TCar, TCdr> {
+    public car: TCar;
+    public cdr: TCdr;
+
+    constructor(car: TCar, cdr: TCdr) {
+        this.car = car;
+        this.cdr = cdr;
+    }
+}
+
+window.onload = () => {
 };
